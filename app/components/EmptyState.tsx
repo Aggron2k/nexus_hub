f692d517444863bc257967f25b63b37b170ced85
Horváth Krisztián Sándor <h158795@stud.u-szeged.hu>
2024-09-30 14:42:14 +0200

    Chat: starter page

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,13 @@
+const EmptyState = () =>{
+    return (
+        <div className="px-4 py-10 sm:px-6 lg:py-8 h-full flex justify-center items-center bg-gray-100">
+            <div className="text-center items-center flex flex-col">
+                <h3 className="mt-2 text-2xl font-semibold text-gray-900">
+                    Válasz ki egy chatet vagy kezdj el egy új beszélgetést
+                </h3>
+            </div>
+        </div>
+    );
+}
+
+export default EmptyState;
