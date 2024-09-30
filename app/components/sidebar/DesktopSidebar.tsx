35a9a3f58b5fa912fc75d35fbb5975a48d4f5d1c
Horváth Krisztián Sándor <h158795@stud.u-szeged.hu>
2024-09-30 17:14:52 +0200

    add: next-superjson GetCurrentSessionUser -  to show avatar at sidebar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -2,8 +2,18 @@
 import { useState } from "react";
 import useRoutes from "@/app/hooks/useRoutes";
 import DesktopItem from "./DesktopItem";
+import Avatar from "@/app/components/Avatar"
 
-const DesktopSidebar = () => {
+import { User } from "@prisma/client";
+
+interface DesktopSidebarProps {
+ currentUser: User
+}
+
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+    currentUser
+}) => {
     const routes = useRoutes();
     const [isOpen, setIsOpen] = useState(false);
 
@@ -20,6 +30,11 @@ const DesktopSidebar = () => {
                     onClick={item.onClick}
                     />
                 ))}</ul>
+            </nav>
+            <nav className="mt-4 flex flex-col justify-between items-center">
+                <div onClick={() => setIsOpen(true)} className="cursor-pointer hover:opacity-75 transition">
+                    <Avatar user={currentUser}/>
+                </div>
             </nav>
         </div>
     );
--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,12 +1,15 @@
 import DesktopSidebar from "./DesktopSidebar";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 async function Sidebar({children}:{
     children: React.ReactNode;
 }){
+    const currentUser = await getCurrentUser();
+
     return (
         <div className="h-full">
+            <DesktopSidebar currentUser={currentUser!} />
             <main className="lg:pl-20 h-full">
-                <DesktopSidebar/>
             {children}
             </main>
         </div>
