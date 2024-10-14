e05d6c47006e66258462d0bfaf7822c24088fecb
Horváth Krisztián Sándor <h158795@stud.u-szeged.hu>
2024-10-14 14:54:46 +0200

    Conversations logic & some tweak to write useres names

--- app/components/UserBox.tsx
+++ app/components/UserBox.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import Avatar from "./Avatar";
 import axios from "axios";
+import { userAgent } from "next/server";
 
 interface UserBoxProps {
     data: User;
@@ -30,7 +31,14 @@ const UserBox: React.FC<UserBoxProps> = ({
         });
     }, [data, router]);
     return (
-        <div onClick={handleClick} className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition, curosr-pointer"><Avatar user={data}/></div>
+        <div onClick={handleClick} className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition, curosr-pointer">
+            <Avatar user={data}/>
+            <div className="focus-outline-none">
+                <span className="felx justify-between items-center mb-1">
+                    <p className="text-sm font-medium text-gray-900">{data.name}</p>
+                </span>
+            </div>
+        </div>
     );
 }
 
