d73f8376739e4fcee1205804bd0ef0be5bc14fa8
Horváth Krisztián Sándor <h158795@stud.u-szeged.hu>
2024-11-05 17:37:59 +0100

    [Package add]: react-spinner [Fornt-end]: Add LoadingModal and func, every Api call can see if its loading

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -5,6 +5,7 @@ import { User } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 
 interface UserBoxProps {
   data: User
@@ -20,29 +21,34 @@ const UserBox: React.FC<UserBoxProps> = ({
   const handleClick = useCallback(() => {
     setIsLoading(true);
 
-    axios.post('/api/conversations', { 
+    axios.post('/api/conversations', {
       userId: data.id
     })
-    .then((data) => {
-      router.push(`/conversations/${data.data.id}`);
-    })
-    .finally(() => setIsLoading(false));
+      .then((data) => {
+        router.push(`/conversations/${data.data.id}`);
+      })
+      .finally(() => setIsLoading(false));
 
   }, [data, router]);
-  return ( 
-    <div onClick={handleClick} className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer">
-      <Avatar user={data} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <div className="flex justify-between items-center mb-1">
-            <p className="text-sm font-medium text-gray-900">
-              {data.name}
-            </p>
+  return (
+    <>
+      {isLoading && (
+        <LoadingModal />
+      )}
+      <div onClick={handleClick} className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer">
+        <Avatar user={data} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <div className="flex justify-between items-center mb-1">
+              <p className="text-sm font-medium text-gray-900">
+                {data.name}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-   );
+    </>
+  );
 }
- 
+
 export default UserBox;
